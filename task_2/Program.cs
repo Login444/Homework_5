d02a9467cc824ea420dc2fc03a0569ae1da4d535
Artemiy <birukovartemij@gmail.com>
2023-01-21 17:34:09 +0300

    task2 completed

---
+++ task_2/Program.cs
@@ -0,0 +1,35 @@
+﻿void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+int[] CreateRandomArray(int N)
+{
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(100, 1000);
+    }
+    return RandomArray;
+}
+
+void SumOfOddNums(int[] sum)
+{
+    int sum1 = 0;
+    for (int i = 0; i < sum.Length; i = i + 2)
+    {
+        sum1 = sum1 + sum[i];
+    }
+    Console.WriteLine($"Сумма чисел стоящих на нечетных позициях равна {sum1}");
+}
+
+Console.WriteLine("Сколько чисел будем считать?");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateRandomArray(a);
+Console.WriteLine("Вот ваш массив: ");
+ShowArray(array);
+SumOfOddNums(array);
